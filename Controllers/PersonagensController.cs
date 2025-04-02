e9424a46e2cf1a1a167f246760c62c99a90824ea
Sara Marcely <saradomacario@gmail.com>
2025-04-02 20:37:31 -0300

    DS - RpgApi 02042025

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RpgApi.Data;
+using RpgApi.Models;
 
 namespace RpgApi.Controllers
 {
@@ -11,6 +14,27 @@ namespace RpgApi.Controllers
     [Route("[controller]")]
     public class PersonagensController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+
+        //ctor - construtor
+        public PersonagensController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            try 
+            {
+                Personagem p = await _context.TB_PERSONAGENS 
+                    .FirstOrDefaultAsync(pBusca => pBusca.Id == id);
+                return Ok(p);    
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
