0147682a7d1a40b7a0800c8e4cf90c533d819772
Sara Marcely <saradomacario@gmail.com>
2025-04-02 22:27:13 -0300

    DS - RpgApi 02042025

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -22,7 +22,7 @@ namespace RpgApi.Controllers
             _context = context;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}")] //Buscar pelo id
         public async Task<IActionResult> GetSingle(int id)
         {
             try 
@@ -36,5 +36,77 @@ namespace RpgApi.Controllers
                 return BadRequest(ex.Message);
             }
         }
+        
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<Personagem> lista = await _context.TB_PERSONAGENS.ToListAsync();
+                return Ok(lista);        
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Personagem novoPersonagem)
+        {
+            try
+            {
+                if (novoPersonagem.PontosVida > 100)
+                {
+                    throw new System.Exception("Pontos de vida não pode ser maior que 100.");
+                }
+                await _context.TB_PERSONAGENS.AddAsync(novoPersonagem);
+                await _context.SaveChangesAsync();
+
+                return Ok(novoPersonagem);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Personagem novoPersonagem)
+        {
+            try
+            {
+                if (novoPersonagem.PontosVida > 100)
+                {
+                    throw new System.Exception("Pontos de vida nao pode ser maior que 100.");
+                }
+                _context.TB_PERSONAGENS.Update(novoPersonagem);
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok(linhasAfetadas);
+            }
+
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                Personagem pRemover = await _context.TB_PERSONAGENS.FirstOrDefaultAsync(p => p. Id == id);
+
+                _context.TB_PERSONAGENS.Remove(pRemover);
+                int linhasAfetadas = await _context.SaveChangesAsync();
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }    
     }
 }
